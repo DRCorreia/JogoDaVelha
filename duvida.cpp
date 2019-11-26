eba6b00ada598c3d97cebcc45129f0e9c68f20fe
Daniel Correia <48036778+DRCorreia@users.noreply.github.com>
2019-11-25 21:05:04 -0300

    Update duvida.cpp

--- duvida.cpp
+++ duvida.cpp
@@ -5,20 +5,21 @@
 int main()
 {
 	int numero;
-	char aux[20];
-	printf("insira um numero de 1 a 3:\n");
-	scanf("%c",&aux);
-	numero = atoi(aux);
-	if(numero==0)
+	char aux;
+	
+	for (;;)
 	{
-		while(numero==0)
+		printf("insira um numero de 1 a 3:\n");
+		scanf("%c",&aux);
+		if(aux > '3' || aux < '1') 
 		{
-			printf("insira um numero de 1 a 3:\n");
-			scanf("%c",&aux);
-			numero = atoi(aux);
+			fflush(stdin);
+			continue;
 		}
+		numero = aux-'0';
+		break;	
 	}
-	else printf("numero valido");
 	return 0;
 }
+
 
